cc99c3fe9505ddddf622d14302fd9d3fe360bfc1
Arthur <arthuroaraujo@gmail.com>
2022-08-30 15:04:58 -0300

    Req 1

---
+++ src/pages/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (<div data-testid="page-not-found">NotFound</div>);
+  }
+}
+
+export default ProfileEdit;
