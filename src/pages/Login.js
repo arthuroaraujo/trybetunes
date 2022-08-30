2777d6add712f5df2e370d42b5198632add989c6
Arthur <arthuroaraujo@gmail.com>
2022-08-30 19:30:52 -0300

    Req 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+// import { Redirect } from 'react-router-dom';
+import Proptypes from 'prop-types';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends React.Component {
+  state = {
+    inputName: '',
+    isDisabled: true,
+    loading: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      inputName: value,
+    }, () => {
+      const nameLength = value.length;
+      const minNumber = 3;
+      const validation = nameLength >= minNumber;
+      this.setState({ isDisabled: !validation });
+    });
+  };
+
+  handleClick = () => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const { inputName } = this.state;
+      await createUser({ name: inputName });
+      const { history } = this.props;
+      history.push('/search');
+    });
+  };
+
   render() {
-    return (<div data-testid="page-login">Login</div>);
+    const { inputName, isDisabled, loading } = this.state;
+    return (
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="name">
+            Nome:
+            <input
+              value={ inputName }
+              type="text"
+              data-testid="login-name-input"
+              name="inputName"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isDisabled }
+            onClick={ this.handleClick }
+          >
+            Entrar
+
+          </button>
+        </form>
+        {loading && (
+          <Loading />
+        )}
+      </div>
+    );
   }
 }
+Login.propTypes = {
+  history: Proptypes.shape({
+    push: Proptypes.func,
+  }).isRequired,
+};
 
 export default Login;
