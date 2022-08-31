1d051c4a2e267cd2b53ee6afc8e8a844d92792c7
Arthur <arthuroaraujo@gmail.com>
2022-08-31 15:10:46 -0300

    Req 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Loading from '../pages/Loading';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
-  // state = {
-  //   user: '',
-  //   loading: false,
-  // };
+  state = {
+    user: '',
+    loading: false,
+  };
 
   componentDidMount() {
-
+    this.setState({
+      loading: true,
+    }, async () => {
+      const user = await getUser();
+      this.setState({ user, loading: false });
+    });
   }
 
   render() {
+    const { user, loading } = this.state;
     return (
       <header data-testid="header-component">
-        Header
+        {loading ? (<Loading />) : (<p data-testid="header-user-name">{user.name}</p>)}
         <Link to="/search" data-testid="link-to-search"> Search </Link>
         <Link to="/favorites" data-testid="link-to-favorites"> Favorites </Link>
         <Link to="/profile" data-testid="link-to-profile"> Profile </Link>
-        {/* {loading ? (<Loading />) : (<p data-testid="header-user-name">Alo</p>)} */}
 
       </header>
     );
